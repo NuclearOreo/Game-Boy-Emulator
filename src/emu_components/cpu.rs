c65a66b112efe6f7d8e413323ab7f54f863f1ad6
Ussama Azam <nuclearoreo@gmail.com>
2023-05-23 22:59:04 -0400

    Adding sdl2

---
+++ src/emu_components/cpu.rs
@@ -0,0 +1,8 @@
+#[allow(dead_code)]
+pub fn cpu_init() {}
+
+#[allow(dead_code)]
+pub fn cpu_step() -> bool {
+    println!("Cpu not yet implemented,");
+    false
+}
---
+++ src/emu_components/ppu.rs
@@ -0,0 +1,5 @@
+#[allow(dead_code)]
+pub fn ppu_init() {}
+
+#[allow(dead_code)]
+pub fn ppu_tick() {}
