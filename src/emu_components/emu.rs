54265fabf648b5418460b3b7c92131fd0406d872
Ussama Azam <nuclearoreo@gmail.com>
2023-05-26 15:54:15 -0400

    Basic run emu

--- src/emu_components/emu.rs
+++ src/emu_components/emu.rs
@@ -1,3 +1,6 @@
+use crate::emu_components::cpu::{cpu_init, cpu_step};
+use sdl2;
+
 pub struct EmuContext {
     paused: bool,
     running: bool,
@@ -14,17 +17,45 @@ pub fn emu_get_context() -> &'static mut EmuContext {
     unsafe { &mut CTX }
 }
 
-pub fn emu_run(args: Vec<String>) -> i32 {
+fn delay(ms: u32) {
+    todo!();
+}
+
+pub fn emu_run(args: Vec<String>) {
     if args.len() < 2 {
-        println!("Usage: emu <rom_file>");
-        return -1;
+        panic!("Usage: emu <rom_file>");
     }
 
     let rom_file = &args[1];
-    if rom_file.len() > 0 {
-        println!("Failed to load ROM file: {}", rom_file);
-        return -2;
+    if rom_file.len() == 0 {
+        panic!("Failed to load ROM file: {}", rom_file);
     }
 
-    0
+    println!("Cart Loaded..");
+
+    sdl2::init().expect("Expecting SDL2 to work");
+    println!("SDL INIT");
+    // sdl2::ttf::init().expect("Expecting SDL2 ttf to work");
+    println!("TTF INIT");
+
+    cpu_init();
+
+    unsafe {
+        CTX.running = true;
+        CTX.paused = false;
+        CTX.ticks = 0;
+
+        while CTX.running {
+            if CTX.paused {
+                delay(10);
+                continue;
+            }
+
+            if !cpu_step() {
+                println!("CPU Stopped");
+            }
+
+            CTX.ticks += 1;
+        }
+    }
 }
