46fbfabc736c34a98756b2ac226786fc06079bc2
Ussama Azam <nuclearoreo@gmail.com>
2023-06-17 21:18:28 -0400

    Finished up fetch data

--- src/emu_components/cpu_fetch.rs
+++ src/emu_components/cpu_fetch.rs
@@ -1,6 +1,6 @@
 use crate::emu_components::bus::bus_read;
 use crate::emu_components::cpu::cpu_get_context;
-use crate::emu_components::cpu_util::cpu_read_reg;
+use crate::emu_components::cpu_util::{cpu_read_reg, cpu_set_reg};
 use crate::emu_components::emu::emu_cycles;
 use crate::emu_components::instructions::{AddrMode, RegType};
 
@@ -56,7 +56,84 @@ pub unsafe fn fetch_data() {
         AddrMode::AM_R_HLI => {
             ctx.fetched_data = bus_read(cpu_read_reg(ctx.cur_inst.reg_2)) as u16;
             emu_cycles(1);
-            // Need cpu_set_reg
+            cpu_set_reg(RegType::RT_HL, cpu_read_reg(RegType::RT_HL) + 1);
+        }
+        AddrMode::AM_R_HLD => {
+            ctx.fetched_data = bus_read(cpu_read_reg(ctx.cur_inst.reg_2)) as u16;
+            emu_cycles(1);
+            cpu_set_reg(RegType::RT_HL, cpu_read_reg(RegType::RT_HL) - 1);
+        }
+        AddrMode::AM_HLI_R => {
+            ctx.fetched_data = cpu_read_reg(ctx.cur_inst.reg_2);
+            ctx.mem_dest = cpu_read_reg(ctx.cur_inst.reg_1);
+            ctx.dest_is_mem = true;
+            cpu_set_reg(RegType::RT_HL, cpu_read_reg(RegType::RT_HL) + 1);
+        }
+        AddrMode::AM_HLD_R => {
+            ctx.fetched_data = cpu_read_reg(ctx.cur_inst.reg_2);
+            ctx.mem_dest = cpu_read_reg(ctx.cur_inst.reg_1);
+            ctx.dest_is_mem = true;
+            cpu_set_reg(RegType::RT_HL, cpu_read_reg(RegType::RT_HL) - 1);
+        }
+        AddrMode::AM_R_A8 => {
+            ctx.fetched_data = bus_read(ctx.regs.pc) as u16;
+            emu_cycles(1);
+            ctx.regs.pc += 1;
+        }
+        AddrMode::AM_A8_R => {
+            ctx.mem_dest = bus_read(ctx.regs.pc) as u16 | 0xFF00;
+            ctx.dest_is_mem = true;
+            emu_cycles(1);
+            ctx.regs.pc += 1;
+        }
+        AddrMode::AM_HL_SPR => {
+            ctx.fetched_data = bus_read(ctx.regs.pc) as u16;
+            emu_cycles(1);
+            ctx.regs.pc += 1;
+        }
+        AddrMode::AM_D8 => {
+            ctx.fetched_data = bus_read(ctx.regs.pc) as u16;
+            emu_cycles(1);
+            ctx.regs.pc += 1;
+        }
+        AddrMode::AM_A16_R | AddrMode::AM_D16_R => {
+            let lo = bus_read(ctx.regs.pc);
+            emu_cycles(1);
+
+            let hi = bus_read(ctx.regs.pc + 1);
+            emu_cycles(1);
+
+            ctx.mem_dest = lo as u16 | ((hi as u16) << 8);
+            ctx.dest_is_mem = true;
+
+            ctx.regs.pc += 2;
+            ctx.fetched_data = cpu_read_reg(ctx.cur_inst.reg_2);
+        }
+        AddrMode::AM_MR_D8 => {
+            ctx.fetched_data = bus_read(ctx.regs.pc) as u16;
+            emu_cycles(1);
+            ctx.regs.pc += 1;
+            ctx.mem_dest = cpu_read_reg(ctx.cur_inst.reg_1);
+            ctx.dest_is_mem = true;
+        }
+        AddrMode::AM_MR => {
+            ctx.mem_dest = cpu_read_reg(ctx.cur_inst.reg_1);
+            ctx.dest_is_mem = true;
+            ctx.fetched_data = bus_read(cpu_read_reg(ctx.cur_inst.reg_1)) as u16;
+            emu_cycles(1);
+        }
+        AddrMode::AM_R_A16 => {
+            let lo = bus_read(ctx.regs.pc);
+            emu_cycles(1);
+
+            let hi = bus_read(ctx.regs.pc + 1);
+            emu_cycles(1);
+
+            let addr = lo as u16 | ((hi as u16) << 8);
+
+            ctx.regs.pc += 2;
+            ctx.fetched_data = bus_read(addr) as u16;
+            emu_cycles(1);
         }
         _ => panic!("Unknown Addressing mode"),
     }
